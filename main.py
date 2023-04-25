2b7b9c4518073680936c03f20ae3b1b56f2c3a2b
AomiVel <kaitoyoutube@icloud.com>
2023-04-25 20:48:56 +0900

    設置可能場所の検知機能を追加

--- main.py
+++ main.py
@@ -5,12 +5,11 @@
 me = 1
 
 
-if not me in (1, 2):
-    print("Select your color")
-    print("1 - White")
-    print("2 - Black")
-    me = input("Number[1/2]:")
-
+match me:
+    case 1:
+        enemy = 2
+    case 2:
+        enemy = 1
 
 white = '○'
 black = '●'
@@ -24,15 +23,21 @@ placeable_text = "\033[46m"
 
 
 board = [
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 2, 0, 0, 0],
-    [0, 0, 0, 2, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
+#    0  1  2  3  4  5  6  7
+    [0, 0, 0, 0, 0, 0, 0, 0], # 0
+    [0, 0, 0, 0, 0, 0, 0, 0], # 1
+    [0, 0, 0, 0, 0, 0, 0, 0], # 2
+    [0, 0, 0, 1, 2, 0, 0, 0], # 3
+    [0, 0, 0, 2, 1, 0, 0, 0], # 4
+    [0, 0, 0, 0, 0, 0, 0, 0], # 5
+    [0, 0, 0, 0, 0, 0, 0, 0], # 6
+    [0, 0, 0, 0, 0, 0, 0, 0], # 7
 ]
+
+# import random
+# for a in range(8):
+#     for b in range(8):
+#         board[a][b] = random.randint(0, 2)
 
 def show_board():
     for row in board:
@@ -54,10 +59,262 @@ def show_board():
 
     print(gb_text, "+---+---+---+---+---+---+---+---+", end_text, sep='')
 
+# ↑ ↗ → ↘ ↓ ↙ ← ↖
+def patch_placeable():
+    global board
+    placeable_list = []
+    for y, row in enumerate(board):
+        for x, col in enumerate(row):
+            if check_placeable(x, y):
+                placeable_list.append((x, y))
+            
+    
+    for x, y in placeable_list:
+        board[y][x] = 3
 
-def patch_placeable(color: int):
-    for rindex, row in enumerate(board):
-        for cindex, col in enumerate(row):
-            if cindex in []
+
+def check_placeable(x, y):
+    return any([
+        check_above(x, y),
+        check_upright(x, y),
+        check_right(x, y),
+        check_bottomright(x, y),
+        check_bottom(x, y),
+        check_bottomleft(x, y),
+        check_left(x, y),
+        check_upleft(x, y)
+    ])
+
+def check_above(x, y):
+    if y in [0, 1]:
+        return False
+    
+    # 空きじゃなければ不可
+    if board[y][x] != 0:
+        return False
+
+    # 設置確認場所より上のスペースの状態を取得
+    disk_list = []
+    for r in range(y):
+        disk_list.append(board[y - (r+1)][x])
+    
+    # 設置確認場所の真上が敵のもの以外だったら不可
+    if disk_list[0] != enemy:
+        return False
+    
+    # 真上のを削除
+    del disk_list[0]
+
+    for disk in disk_list:
+        if disk == enemy:
+            continue
+        elif disk == me:
+            return True
+        elif disk == 0:
+            return False
+    return False
+
+def check_upright(x, y):
+    if y in [0, 1]:
+        return False
+    if x in [6, 7]:
+        return False
+    
+    # 空きじゃなければ不可
+    if board[y][x] != 0:
+        return False
+
+    up = y
+    right = 7 - x
+
+    disk_list = []
+    for r in range(min((up, right))):
+        disk_list.append(board[y - (r+1)][x + r + 1])
+    
+    if disk_list[0] != enemy:
+        return False
+    
+    del disk_list[0]
+
+    for disk in disk_list:
+        if disk == enemy:
+            continue
+        elif disk == me:
+            return True
+        elif disk == 0:
+            return False
+    return False
+
+def check_right(x, y):
+    if x in [6, 7]:
+        return False
+    
+    if board[y][x] != 0:
+        return False
+
+    disk_list = []
+    for r in range(7 - x):
+        disk_list.append(board[y][x + r + 1])
+    
+    if disk_list[0] != enemy:
+        return False
+    
+    del disk_list[0]
+
+    for disk in disk_list:
+        if disk == enemy:
+            continue
+        elif disk == me:
+            return True
+        elif disk == 0:
+            return False
+    return False
+
+def check_bottomright(x, y):
+    if y in [6, 7]:
+        return False
+    if x in [6, 7]:
+        return False
+    
+    # 空きじゃなければ不可
+    if board[y][x] != 0:
+        return False
+
+    bottom = 7 - y
+    right = 7 - x
+
+    disk_list = []
+    for r in range(min((bottom, right))):
+        disk_list.append(board[y + r + 1][x + r + 1])
+    
+    if disk_list[0] != enemy:
+        return False
+    
+    del disk_list[0]
+
+    for disk in disk_list:
+        if disk == enemy:
+            continue
+        elif disk == me:
+            return True
+        elif disk == 0:
+            return False
+    return False
+
+def check_bottom(x, y):
+    if y in [6, 7]:
+        return False
+    
+    # 空きじゃなければ不可
+    if board[y][x] != 0:
+        return False
+
+    # 設置確認場所より上のスペースの状態を取得
+    disk_list = []
+    for r in range(7 - y):
+        disk_list.append(board[y + r + 1][x])
+    
+    # 設置確認場所の真上が敵のもの以外だったら不可
+    if disk_list[0] != enemy:
+        return False
+    
+    # 真上のを削除
+    del disk_list[0]
+
+    for disk in disk_list:
+        if disk == enemy:
+            continue
+        elif disk == me:
+            return True
+        elif disk == 0:
+            return False
+    return False
+
+def check_bottomleft(x, y):
+    if y in [6, 7]:
+        return False
+    if x in [0, 1]:
+        return False
+    
+    # 空きじゃなければ不可
+    if board[y][x] != 0:
+        return False
+
+    bottom = 7 - y
+    left = x
+
+    disk_list = []
+    for r in range(min((bottom, left))):
+        disk_list.append(board[y + r + 1][x - (r+1)])
+    
+    if disk_list[0] != enemy:
+        return False
+    
+    del disk_list[0]
+
+    for disk in disk_list:
+        if disk == enemy:
+            continue
+        elif disk == me:
+            return True
+        elif disk == 0:
+            return False
+    return False
+
+def check_left(x, y):
+    if x in [0, 1]:
+        return False
+    
+    if board[y][x] != 0:
+        return False
+
+    disk_list = []
+    for r in range(x):
+        disk_list.append(board[y][x - (r + 1)])
+    
+    if disk_list[0] != enemy:
+        return False
+    
+    del disk_list[0]
+
+    for disk in disk_list:
+        if disk == enemy:
+            continue
+        elif disk == me:
+            return True
+        elif disk == 0:
+            return False
+    return False
+
+def check_upleft(x, y):
+    if y in [0, 1]:
+        return False
+    if x in [0, 1]:
+        return False
+    
+    # 空きじゃなければ不可
+    if board[y][x] != 0:
+        return False
+
+    disk_list = []
+    for r in range(min((x, y))):
+        disk_list.append(board[y - (r+1)][x - (r+1)])
+    
+    if disk_list[0] != enemy:
+        return False
+    
+    del disk_list[0]
+
+    for disk in disk_list:
+        if disk == enemy:
+            continue
+        elif disk == me:
+            return True
+        elif disk == 0:
+            return False
+    return False
+
+
 show_board()
-
+patch_placeable()
+show_board()
